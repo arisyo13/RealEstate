ed3a482bdb4b291ad76426312c258c424ea29885
arisyo13 <51513565+arisyo13@users.noreply.github.com>
2021-03-28 00:59:25 +0100

    added fragments and navigation for those

--- app/src/main/java/com/example/realestate/MainActivity.kt
+++ app/src/main/java/com/example/realestate/MainActivity.kt
@@ -2,10 +2,18 @@ package com.example.realestate
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import androidx.navigation.NavController
+import androidx.navigation.Navigation
+import androidx.navigation.ui.NavigationUI
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val bottomNavigationView: BottomNavigationView = findViewById(R.id.nav)
+        val navController: NavController = Navigation.findNavController(this, R.id.nav_host_fragment)
+        NavigationUI.setupWithNavController(bottomNavigationView, navController)
     }
 }
